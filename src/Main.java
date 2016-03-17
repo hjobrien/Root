a045d29c72f7cac7d403f7addc3f8ffa5898d40b
Liam Kreiss <liam.kreiss@gmail.com>
2016-03-17 09:22:02 -0700

    added letters array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 
 public class Main {
+	
+	//should contain all the letters in the player's pile PLUS the tile on the board that
+	//you wish to use in your word
+	char[] letters = new char[]{
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+	};
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
