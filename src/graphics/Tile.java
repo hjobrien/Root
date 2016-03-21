2ab6f0fd8530bf0546169b7af39e6feefa2819f0
Hank <hank.obrien900@icloud.com>
2016-03-21 09:53:56 -0700

    added tile

---
+++ src/graphics/Tile.java
@@ -0,0 +1,30 @@
+package graphics;
+
+public class Tile {
+	private int type;
+	
+	public Tile(int type){
+		this.type = type;
+	}
+	
+	public String getColor(){
+		switch(type){
+		case 0:
+			return "black";
+		case 1:
+			return "lightgrey";
+		case 2:
+			return "lightblue";
+		case 3:
+			return "blue";
+		case 4:
+			return "pink";
+		case 5:
+			return "red";
+		case 7:
+			return "hotpink";
+		default:
+			return "green";
+		}
+	}
+}
